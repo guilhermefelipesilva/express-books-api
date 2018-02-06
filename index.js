edcea4c9097e40d7b66837033df744fd370acbbb
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2018-02-06 21:13:18 -0200

    adição de eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import app from './app'
+import app from './app';
 
 app.listen(app.get('port'), () => {
-    console.log(`Rodando na porta ${app.get('port')}`)
-})
+  console.log(`Rodando na porta ${app.get('port')}`);
+});
