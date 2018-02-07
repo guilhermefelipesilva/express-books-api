f2c60ef52c253882350c9202b9c839bd6f813f8a
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2018-02-07 09:36:19 -0200

    teste de unidade com mock do funcão que busca todos os livros

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,24 @@
+const defaultResponse = (data, statusCode = 200) => ({
+  data,
+  statusCode,
+});
+
+const errorResponse = (message, statusCode = 400) => ({
+  error: message,
+  statusCode,
+});
+
 class BooksController {
   constructor(Books) {
     this.Books = Books;
   }
+
+  getAll() {
+    return this.Books.findAll({})
+      .then(result => defaultResponse(result))
+      .catch(error => errorResponse(error.message));
+  }
 }
+
+export default BooksController;
+
